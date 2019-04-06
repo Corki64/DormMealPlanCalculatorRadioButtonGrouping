5adc5f1f139fc780cc8b1833ddf2ef98e3966f70
Luis Cortez <lac0084@auburn.com>
2019-04-06 11:57:24 -0500

    Switched groups to panels - due to getting the border to dissappear.

--- DormMealPlanCalculator/Form1.Designer.cs
+++ DormMealPlanCalculator/Form1.Designer.cs
@@ -33,18 +33,22 @@
                this.FarthingHall_selectionThree = new System.Windows.Forms.RadioButton();
                this.UniversitySuites_selectionFour = new System.Windows.Forms.RadioButton();
                this.label1 = new System.Windows.Forms.Label();
-               this.label2 = new System.Windows.Forms.Label();
-               this.infiniteMeal_1700 = new System.Windows.Forms.RadioButton();
-               this.fourteenMeal_1200 = new System.Windows.Forms.RadioButton();
-               this.sevenMeal_600 = new System.Windows.Forms.RadioButton();
                this.calculateTotalDescrip = new System.Windows.Forms.Label();
                this.calculateTotal = new System.Windows.Forms.Button();
+               this.radioButton11 = new System.Windows.Forms.RadioButton();
+               this.label6 = new System.Windows.Forms.Label();
+               this.radioButton12 = new System.Windows.Forms.RadioButton();
+               this.radioButton13 = new System.Windows.Forms.RadioButton();
+               this.dormPanel = new System.Windows.Forms.Panel();
+               this.mealPanel = new System.Windows.Forms.Panel();
+               this.dormPanel.SuspendLayout();
+               this.mealPanel.SuspendLayout();
                this.SuspendLayout();
                // 
                // AllenHall_selectionOne
                // 
                this.AllenHall_selectionOne.AutoSize = true;
-               this.AllenHall_selectionOne.Location = new System.Drawing.Point(35, 69);
+               this.AllenHall_selectionOne.Location = new System.Drawing.Point(6, 42);
                this.AllenHall_selectionOne.Name = "AllenHall_selectionOne";
                this.AllenHall_selectionOne.Size = new System.Drawing.Size(171, 17);
                this.AllenHall_selectionOne.TabIndex = 4;
@@ -55,7 +59,7 @@
                // PikeHall_selectionTwo
                // 
                this.PikeHall_selectionTwo.AutoSize = true;
-               this.PikeHall_selectionTwo.Location = new System.Drawing.Point(35, 118);
+               this.PikeHall_selectionTwo.Location = new System.Drawing.Point(6, 91);
                this.PikeHall_selectionTwo.Name = "PikeHall_selectionTwo";
                this.PikeHall_selectionTwo.Size = new System.Drawing.Size(169, 17);
                this.PikeHall_selectionTwo.TabIndex = 5;
@@ -66,7 +70,7 @@
                // FarthingHall_selectionThree
                // 
                this.FarthingHall_selectionThree.AutoSize = true;
-               this.FarthingHall_selectionThree.Location = new System.Drawing.Point(35, 164);
+               this.FarthingHall_selectionThree.Location = new System.Drawing.Point(6, 137);
                this.FarthingHall_selectionThree.Name = "FarthingHall_selectionThree";
                this.FarthingHall_selectionThree.Size = new System.Drawing.Size(186, 17);
                this.FarthingHall_selectionThree.TabIndex = 6;
@@ -77,7 +81,7 @@
                // UniversitySuites_selectionFour
                // 
                this.UniversitySuites_selectionFour.AutoSize = true;
-               this.UniversitySuites_selectionFour.Location = new System.Drawing.Point(35, 208);
+               this.UniversitySuites_selectionFour.Location = new System.Drawing.Point(6, 181);
                this.UniversitySuites_selectionFour.Name = "UniversitySuites_selectionFour";
                this.UniversitySuites_selectionFour.Size = new System.Drawing.Size(205, 17);
                this.UniversitySuites_selectionFour.TabIndex = 7;
@@ -88,58 +92,16 @@
                // label1
                // 
                this.label1.AutoSize = true;
-               this.label1.Location = new System.Drawing.Point(32, 37);
+               this.label1.Location = new System.Drawing.Point(3, 10);
                this.label1.Name = "label1";
                this.label1.Size = new System.Drawing.Size(217, 13);
                this.label1.TabIndex = 8;
                this.label1.Text = "Please select the dorm you will be staying at:";
                // 
-               // label2
-               // 
-               this.label2.AutoSize = true;
-               this.label2.Location = new System.Drawing.Point(373, 37);
-               this.label2.Name = "label2";
-               this.label2.Size = new System.Drawing.Size(144, 13);
-               this.label2.TabIndex = 9;
-               this.label2.Text = "Please select your meal plan:";
-               // 
-               // infiniteMeal_1700
-               // 
-               this.infiniteMeal_1700.AutoSize = true;
-               this.infiniteMeal_1700.Location = new System.Drawing.Point(376, 164);
-               this.infiniteMeal_1700.Name = "infiniteMeal_1700";
-               this.infiniteMeal_1700.Size = new System.Drawing.Size(200, 17);
-               this.infiniteMeal_1700.TabIndex = 12;
-               this.infiniteMeal_1700.TabStop = true;
-               this.infiniteMeal_1700.Text = "Unlimited meals: $1,700 per semester";
-               this.infiniteMeal_1700.UseVisualStyleBackColor = true;
-               // 
-               // fourteenMeal_1200
-               // 
-               this.fourteenMeal_1200.AutoSize = true;
-               this.fourteenMeal_1200.Location = new System.Drawing.Point(376, 118);
-               this.fourteenMeal_1200.Name = "fourteenMeal_1200";
-               this.fourteenMeal_1200.Size = new System.Drawing.Size(216, 17);
-               this.fourteenMeal_1200.TabIndex = 11;
-               this.fourteenMeal_1200.TabStop = true;
-               this.fourteenMeal_1200.Text = "14 meals per week: $1,200 per semester";
-               this.fourteenMeal_1200.UseVisualStyleBackColor = true;
-               // 
-               // sevenMeal_600
-               // 
-               this.sevenMeal_600.AutoSize = true;
-               this.sevenMeal_600.Location = new System.Drawing.Point(376, 69);
-               this.sevenMeal_600.Name = "sevenMeal_600";
-               this.sevenMeal_600.Size = new System.Drawing.Size(201, 17);
-               this.sevenMeal_600.TabIndex = 10;
-               this.sevenMeal_600.TabStop = true;
-               this.sevenMeal_600.Text = "7 meals per week: $600 per semester";
-               this.sevenMeal_600.UseVisualStyleBackColor = true;
-               // 
                // calculateTotalDescrip
                // 
                this.calculateTotalDescrip.AutoSize = true;
-               this.calculateTotalDescrip.Location = new System.Drawing.Point(151, 283);
+               this.calculateTotalDescrip.Location = new System.Drawing.Point(83, 240);
                this.calculateTotalDescrip.Name = "calculateTotalDescrip";
                this.calculateTotalDescrip.Size = new System.Drawing.Size(321, 13);
                this.calculateTotalDescrip.TabIndex = 13;
@@ -147,31 +109,93 @@
                // 
                // calculateTotal
                // 
-               this.calculateTotal.Location = new System.Drawing.Point(216, 314);
+               this.calculateTotal.Location = new System.Drawing.Point(148, 271);
                this.calculateTotal.Name = "calculateTotal";
                this.calculateTotal.Size = new System.Drawing.Size(162, 23);
                this.calculateTotal.TabIndex = 14;
                this.calculateTotal.Text = "Calculate Homelessness";
                this.calculateTotal.UseVisualStyleBackColor = true;
                // 
+               // radioButton11
+               // 
+               this.radioButton11.AutoSize = true;
+               this.radioButton11.Location = new System.Drawing.Point(15, 42);
+               this.radioButton11.Name = "radioButton11";
+               this.radioButton11.Size = new System.Drawing.Size(201, 17);
+               this.radioButton11.TabIndex = 10;
+               this.radioButton11.TabStop = true;
+               this.radioButton11.Text = "7 meals per week: $600 per semester";
+               this.radioButton11.UseVisualStyleBackColor = true;
+               // 
+               // label6
+               // 
+               this.label6.AutoSize = true;
+               this.label6.Location = new System.Drawing.Point(12, 10);
+               this.label6.Name = "label6";
+               this.label6.Size = new System.Drawing.Size(144, 13);
+               this.label6.TabIndex = 9;
+               this.label6.Text = "Please select your meal plan:";
+               // 
+               // radioButton12
+               // 
+               this.radioButton12.AutoSize = true;
+               this.radioButton12.Location = new System.Drawing.Point(15, 91);
+               this.radioButton12.Name = "radioButton12";
+               this.radioButton12.Size = new System.Drawing.Size(216, 17);
+               this.radioButton12.TabIndex = 11;
+               this.radioButton12.TabStop = true;
+               this.radioButton12.Text = "14 meals per week: $1,200 per semester";
+               this.radioButton12.UseVisualStyleBackColor = true;
+               // 
+               // radioButton13
+               // 
+               this.radioButton13.AutoSize = true;
+               this.radioButton13.Location = new System.Drawing.Point(15, 137);
+               this.radioButton13.Name = "radioButton13";
+               this.radioButton13.Size = new System.Drawing.Size(200, 17);
+               this.radioButton13.TabIndex = 12;
+               this.radioButton13.TabStop = true;
+               this.radioButton13.Text = "Unlimited meals: $1,700 per semester";
+               this.radioButton13.UseVisualStyleBackColor = true;
+               // 
+               // dormPanel
+               // 
+               this.dormPanel.Controls.Add(this.AllenHall_selectionOne);
+               this.dormPanel.Controls.Add(this.label1);
+               this.dormPanel.Controls.Add(this.PikeHall_selectionTwo);
+               this.dormPanel.Controls.Add(this.UniversitySuites_selectionFour);
+               this.dormPanel.Controls.Add(this.FarthingHall_selectionThree);
+               this.dormPanel.Location = new System.Drawing.Point(11, 12);
+               this.dormPanel.Name = "dormPanel";
+               this.dormPanel.Size = new System.Drawing.Size(230, 208);
+               this.dormPanel.TabIndex = 16;
+               // 
+               // mealPanel
+               // 
+               this.mealPanel.Controls.Add(this.label6);
+               this.mealPanel.Controls.Add(this.radioButton11);
+               this.mealPanel.Controls.Add(this.radioButton13);
+               this.mealPanel.Controls.Add(this.radioButton12);
+               this.mealPanel.Location = new System.Drawing.Point(247, 12);
+               this.mealPanel.Name = "mealPanel";
+               this.mealPanel.Size = new System.Drawing.Size(236, 208);
+               this.mealPanel.TabIndex = 17;
+               // 
                // Form1
                // 
                this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
                this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-               this.ClientSize = new System.Drawing.Size(630, 392);
+               this.ClientSize = new System.Drawing.Size(495, 329);
+               this.Controls.Add(this.mealPanel);
+               this.Controls.Add(this.dormPanel);
                this.Controls.Add(this.calculateTotal);
                this.Controls.Add(this.calculateTotalDescrip);
-               this.Controls.Add(this.infiniteMeal_1700);
-               this.Controls.Add(this.fourteenMeal_1200);
-               this.Controls.Add(this.sevenMeal_600);
-               this.Controls.Add(this.label2);
-               this.Controls.Add(this.label1);
-               this.Controls.Add(this.UniversitySuites_selectionFour);
-               this.Controls.Add(this.FarthingHall_selectionThree);
-               this.Controls.Add(this.PikeHall_selectionTwo);
-               this.Controls.Add(this.AllenHall_selectionOne);
                this.Name = "Form1";
                this.Text = "Dorm and Meal Plan Selector";
+               this.dormPanel.ResumeLayout(false);
+               this.dormPanel.PerformLayout();
+               this.mealPanel.ResumeLayout(false);
+               this.mealPanel.PerformLayout();
                this.ResumeLayout(false);
                this.PerformLayout();
 
@@ -184,12 +208,14 @@
           private System.Windows.Forms.RadioButton FarthingHall_selectionThree;
           private System.Windows.Forms.RadioButton UniversitySuites_selectionFour;
           private System.Windows.Forms.Label label1;
-          private System.Windows.Forms.Label label2;
-          private System.Windows.Forms.RadioButton infiniteMeal_1700;
-          private System.Windows.Forms.RadioButton fourteenMeal_1200;
-          private System.Windows.Forms.RadioButton sevenMeal_600;
           private System.Windows.Forms.Label calculateTotalDescrip;
           private System.Windows.Forms.Button calculateTotal;
+          private System.Windows.Forms.RadioButton radioButton11;
+          private System.Windows.Forms.Label label6;
+          private System.Windows.Forms.RadioButton radioButton12;
+          private System.Windows.Forms.RadioButton radioButton13;
+          private System.Windows.Forms.Panel dormPanel;
+          private System.Windows.Forms.Panel mealPanel;
      }
 }
 
